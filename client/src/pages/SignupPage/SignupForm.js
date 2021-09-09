156a763eea379d81109930a0ef35e68010ea3c80
Danielle Brinkley <daniellebrinkley.fs@gmail.com>
2021-09-08 19:12:53 -0600

    create signup page

--- client/src/pages/SignupPage/SignupForm.js
+++ client/src/pages/SignupPage/SignupForm.js
@@ -5,8 +5,24 @@ import "./styles.css";
 
 const SignupForm = () => {
     return (
-        <div>
-            <p>Signup Form</p>
+        <div className="signup-form-ctr">
+             <input 
+                className="input-fields"
+                type="name"
+                value="NAME"
+                />
+            <input 
+                className="input-fields"
+                type="email"
+                value="EMAIL"
+                />
+            <input 
+                className="input-fields"
+                type="password"
+                value="PASSWORD"
+                />
+            <button className="signup-button">SIGN IN</button>
+            <p>Already have an account? Sign in</p>
         </div>
     )
 }
--- client/src/pages/SignupPage/index.js
+++ client/src/pages/SignupPage/index.js
@@ -1,6 +1,15 @@
 import React from "react";
 import SignupForm from "./SignupForm";
+import logo from '../../images/logo.png'
 
-const SignupPage = () => <SignupForm />;
+const SignupPage = () => {
+return (
+    <div className="signup-ctr"> 
+        <img className="logo" src={logo} alt="website logo"/>
+
+        <SignupForm />
+        </div>
+    )
+}
 
 export default SignupPage;
